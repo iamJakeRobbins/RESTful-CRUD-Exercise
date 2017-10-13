ca6af9cb41efb171a1924e500f59684f3fcaa6af
Brooks Patton <brooks.patton@galvanize.com>
2017-10-13 13:44:00 -0600

    patch route created

--- routes/cartoons.js
+++ routes/cartoons.js
@@ -38,6 +38,14 @@ router.put('/:id', (req, res, next) => {
 		.catch(err => next(err));
 });
 
+router.patch('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	db.update(id, req.body)
+		.then(() => res.sendStatus(200))
+		.catch(err => next(err));
+});
+
 // Route for deleting an item
 
 
