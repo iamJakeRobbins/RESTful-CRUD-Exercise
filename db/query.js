49822a591c9a149abb5c4b845c25e049c900f8fe
Jake Robbins <jrobbinsd@gmail.com>
2017-11-17 13:52:09 -0700

    redone again bluggghhh

--- db/query.js
+++ db/query.js
@@ -1,25 +1,29 @@
-var db = require('./connection');
+var knex = require('./connection');
 
 function getAll() {
 	// Get all cartoons and return them to the route
+	return knex('cartoon');
 }
 
 function getOne(id) {
 	// Get one cartoon by the id and return it
+	return knex('cartoon').where('id', id);
 }
 
 function create(cartoon) {
 	// Add a cartoon to the database
+	return knex('cartoon').insert(cartoon)
 }
 
 function update(id, cartoon) {
 	// Update a cartoon with the specified id
+	return knex('cartoon').update(cartoon).where('id', id);
 }
 
 function remove(id) {
 	// Delete the specified cartoon
+	return knex('cartoon').where('id', id).del()
 }
-
 module.exports = {
 	getAll,
 	getOne,
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,20 +1,58 @@
 var express = require('express');
 var router = express.Router();
 // Bring in the queries object that has all of the db query functions
+var db = require('../db/query');
 
 // Create the route for get all
-
+router.get('/', (req, res, next) => {
+	db.getAll()
+		.then(cartoons => res.json(cartoons))
+		.catch(err => next(err));
+});
 
 // Create the route for get one
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id;
 
+	db.getOne(id)
+		.then(cartoon => res.json(cartoon))
+		.catch(err => next(err));
+})
 
 // Create the route for creating an item
-
+router.post('/', (req, res, next) => {
+	db.create(req.body)
+		.then((result) => res.sendStatus(201))
+		.catch(err => {
+			console.log(err);
+			next(err);
+		});
+});
 
 // Create the route for updating
+router.put('/:id', (req, res, next) => {
+	const id = req.params.id;
 
+	db.update(id, req.body)
+		.then(() => res.sendStatus(200))
+		.catch(err => next(err));
+});
+
+router.patch('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	db.update(id, req.body)
+		.then(() => res.sendStatus(200))
+		.catch(err => next(err));
+});
 
 // Route for deleting an item
+router.delete('/:id', (req, res, next) => {
+	const id = req.params.id;
 
+	db.remove(id)
+		.then(() => res.sendStatus(200))
+		.catch(err => next(err));
+});
 
 module.exports = router;
