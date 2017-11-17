c73783bf5f2f9890cceeed217778d5536678fb14
Jake Robbins <jrobbinsd@gmail.com>
2017-11-17 16:14:34 -0700

    ready to deploy

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 
 module.exports = {
 
@@ -5,5 +6,9 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/cartoons'
   },
+	production:{
+		client: 'pg',
+		connection: process.env.DATABASE_URL
+	}
 
 };
